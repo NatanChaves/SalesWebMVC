9ad8e123a69270f76a6bd56e5ea16f710573d6aa
NatanChaves <nataninacio1chaves@gmail.com>
2020-03-15 15:55:12 -0300

    Segundo teste

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -20,5 +20,6 @@ namespace SalesWebMVC
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
