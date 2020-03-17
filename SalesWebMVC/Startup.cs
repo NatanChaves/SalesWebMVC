4f62c01cd22fc3bdb80c162a4d7f88d86f8ed2c3
NatanChaves <nataninacio1chaves@gmail.com>
2020-03-16 21:34:09 -0300

    Theme of bootstrap changed

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMVCContext>(options =>
-                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), b => b.MigrationsAssembly("SalesWebMVC")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+        builder.MigrationsAssembly("SalesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
